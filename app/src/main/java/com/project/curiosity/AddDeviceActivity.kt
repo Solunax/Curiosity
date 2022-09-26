c2090488f740532621da00d86da61993d5d15857
Solunax <rudgh1409@gmail.com>
2022-09-26 19:53:07 +0900

    using MVVM pattern(Server connection part half done) design changed(Main Activity)

--- app/src/main/java/com/project/curiosity/AddDeviceActivity.kt
+++ app/src/main/java/com/project/curiosity/AddDeviceActivity.kt
@@ -1,23 +1,17 @@
 package com.project.curiosity
 
-import android.app.Activity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.project.curiosity.databinding.AddDeviceBinding
-import com.project.curiosity.room.AppDataBase
 import com.project.curiosity.room.Device
-import com.project.curiosity.viewModel.RoomViewModel
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.launch
+import com.project.curiosity.viewModel.ViewModel
 
 class AddDeviceActivity:AppCompatActivity() {
     private lateinit var binding : AddDeviceBinding
-    private val roomViewModel : RoomViewModel by viewModels()
+    private val viewModel : ViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,7 +29,7 @@ class AddDeviceActivity:AppCompatActivity() {
             if(deviceName.text.isNotBlank()){
                 // 이미 있는 이름인지 확인
                 if(!deviceNameList!!.contains(deviceName.text.toString())){
-                    roomViewModel.insertDeviceData(Device(deviceName.text.toString()))
+                    viewModel.insertDeviceData(Device(deviceName.text.toString()))
                     finish()
                 }else{
                     Toast.makeText(applicationContext, "이미 등록된 이름입니다.", Toast.LENGTH_SHORT).show()
--- app/src/main/java/com/project/curiosity/MainActivity.kt
+++ app/src/main/java/com/project/curiosity/MainActivity.kt
@@ -1,26 +1,31 @@
 package com.project.curiosity
 
+import android.annotation.SuppressLint
 import android.content.Intent
+import android.graphics.BlendMode
+import android.graphics.BlendModeColorFilter
+import android.graphics.Color
+import android.graphics.PorterDuff
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.ImageView
 import android.widget.Spinner
 import androidx.activity.viewModels
 import androidx.annotation.RequiresApi
-import androidx.appcompat.content.res.AppCompatResources
+import androidx.core.content.ContextCompat
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 import com.project.curiosity.api.ApiClient
 import com.project.curiosity.databinding.MainActivityBinding
-import com.project.curiosity.fragment.GpsFragment
 import com.project.curiosity.fragment.GraphFragment
 import com.project.curiosity.fragmentAdapter.StateAdapter
 import com.project.curiosity.model.Body
 import com.project.curiosity.model.Request
-import com.project.curiosity.viewModel.RoomViewModel
+import com.project.curiosity.viewModel.ViewModel
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
@@ -33,13 +38,12 @@ class MainActivity : AppCompatActivity(){
     private lateinit var binding : MainActivityBinding
     private lateinit var job:Job
     private val deviceNameList = ArrayList<String>()
-    private val tabIcon = arrayOf(R.drawable.cam, R.drawable.temp, R.drawable.gps)
+    private val tabIcon = arrayOf(R.drawable.cam, R.drawable.temp2, R.drawable.gps)
     private lateinit var deviceSpinner:Spinner
     private var recentBody:Body? = null
-    private var fragmentIndex = 1
-    private val roomViewModel : RoomViewModel by viewModels()
-    var changeGpsFlag = false
+    val viewModel : ViewModel by viewModels()
 
+    @SuppressLint("InflateParams")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = MainActivityBinding.inflate(layoutInflater)
@@ -52,33 +56,49 @@ class MainActivity : AppCompatActivity(){
 
         val manager = supportFragmentManager
         viewPager.adapter = StateAdapter(manager, lifecycle)
-        viewPager.currentItem = 1
 
+        // 우측 상단 추가 버튼 클릭 시
         addDeviceButton.setOnClickListener {
             val intent = Intent(applicationContext, RoverListActivity::class.java)
             intent.putExtra("deviceList", deviceNameList)
             startActivity(intent)
         }
 
+        // TabLayout, ViewPager 연결
         TabLayoutMediator(tabLayout, viewPager){tab, position ->
-            tab.icon = AppCompatResources.getDrawable(applicationContext, tabIcon[position])
+//            변경 전 기존 코드(주석)
+//            tab.icon = AppCompatResources.getDrawable(applicationContext, tabIcon[position])
+            val iconView = layoutInflater.inflate(R.layout.custom_tab_icon, null)
+            iconView.findViewById<ImageView>(R.id.tab_icon).setImageResource(tabIcon[position])
+            setColors(iconView, false)
+
+            tab.customView = iconView
         }.attach()
 
+        // 탭이 select / unselect 시 상단 TabLayout 의 아이콘 색상 변경, CustomView 라서 별도의 리스너 추가가 필요함
         tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener{
-            @RequiresApi(Build.VERSION_CODES.O)
             override fun onTabSelected(tab: TabLayout.Tab?) {
-                fragmentIndex = tab!!.position
+                val view = tab?.customView
+                setColors(view, true)
             }
 
             override fun onTabUnselected(tab: TabLayout.Tab?) {
+                val view = tab?.customView
+                setColors(view, false)
+
             }
 
             override fun onTabReselected(tab: TabLayout.Tab?) {
+
             }
+
         })
 
+        // 상단의 리스너 설정 이후에 설정해야 의도대로 동작함
+        viewPager.currentItem = 1
+
         // 내부 DB의 device DATA 관측
-        roomViewModel.data.observe(this) {
+        viewModel.nameData.observe(this) {
             deviceNameList.clear()
             it.forEach { v ->
                 deviceNameList.add(v.deviceID)
@@ -87,29 +107,19 @@ class MainActivity : AppCompatActivity(){
             deviceSpinner.adapter = adapter
         }
 
+
         // 장치 ID 변경시 GPS 위치 갱신
         deviceSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
             @RequiresApi(Build.VERSION_CODES.O)
             override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
-                try{
-                    changeGpsFlag = true
-                    if(fragmentIndex == 1){
-                        val graphFragment = supportFragmentManager.findFragmentByTag("f1") as GraphFragment
-                        changeDevice(graphFragment)
-                    }
-                    else if(fragmentIndex == 2){
-                        val gpsFragment = supportFragmentManager.findFragmentByTag("f2") as GpsFragment
-                        changeDevice(gpsFragment)
-                    }
-                }catch (e:Exception){
-                    e.printStackTrace()
-                }
+                getData()
             }
 
             override fun onNothingSelected(p0: AdapterView<*>?) {
             }
         }
-        // 10초 주기로 서버에서 데이터를 가져옴
+
+        // ?초 주기로 서버에서 데이터를 가져옴
         val timer = Timer()
         timer.schedule(object: TimerTask(){
             @RequiresApi(Build.VERSION_CODES.O)
@@ -128,38 +138,53 @@ class MainActivity : AppCompatActivity(){
         }
     }
 
-    // 서버에서 최신값 하나만 정보 가져오기
-    @RequiresApi(Build.VERSION_CODES.O)
-    private fun getData() {
-        job = CoroutineScope(Dispatchers.IO).launch {
-            val request = Request(getSpinnerData(), "")
-            val response = ApiClient.getApiClient().getData(request)
-            if(response.isSuccessful && response.body()!!.statusCode == 200){
-                recentBody = response.body()!!.body[0]
-                if(fragmentIndex == 1){
-                    val graphFragment = supportFragmentManager.findFragmentByTag("f1") as GraphFragment
-                    graphFragment.setGraph(recentBody!!)
-                }else if(fragmentIndex == 2){
-                    val gpsFragment = supportFragmentManager.findFragmentByTag("f2") as GpsFragment
-                    gpsFragment.drawRoute(false, recentBody!!)
-                }
+    // 서버에서 최신값 정보 가져오기
+    private fun getData(){
+        val nameData = getSpinnerData()
+        if(nameData != "ERROR")
+            viewModel.getData(Request(nameData, ""))
+    }
+
+    // TabLayout 아이콘 색상 변경 함수
+    private fun setColors(view : View?, flag : Boolean){
+        val icon: ImageView? = view?.findViewById(R.id.tab_icon)
+
+        when(flag){
+            true -> {
+                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q)
+                    icon?.colorFilter = BlendModeColorFilter(ContextCompat.getColor(applicationContext, R.color.myyellow), BlendMode.SRC_IN)
+                else
+                    icon?.setColorFilter(R.color.myyellow, PorterDuff.Mode.SRC_IN)
+            }
+            false ->{
+                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q)
+                    icon?.colorFilter = BlendModeColorFilter(Color.parseColor("#343434"), BlendMode.SRC_IN)
+                else
+                    icon?.setColorFilter(Color.parseColor("#343434"), PorterDuff.Mode.SRC_IN)
             }
         }
     }
 
-    // 장치 변경시 GPS 갱신
-    private fun changeDevice(fragment:GpsFragment) {
-        job = CoroutineScope(Dispatchers.IO).launch {
-            val request = Request(getSpinnerData(), "")
-            val response = ApiClient.getApiClient().getData(request)
-            if(response.isSuccessful && response.body()!!.statusCode == 200){
-                recentBody = response.body()!!.body[0]
-                fragment.drawRoute(true, recentBody!!)
-            }
-        }
-    }
+//    기존 통신 코드
+//    @RequiresApi(Build.VERSION_CODES.O)
+//    private fun getData() {
+//        job = CoroutineScope(Dispatchers.IO).launch {
+//            val request = Request(getSpinnerData(), "")
+//            val response = ApiClient.getApiClient().getData(request)
+//            if(response.isSuccessful && response.body()!!.statusCode == 200){
+//                recentBody = response.body()!!.body[0]
+//                if(fragmentIndex == 1){
+//                    val graphFragment = supportFragmentManager.findFragmentByTag("f1") as GraphFragment
+//                    graphFragment.setGraph(recentBody!!)
+//                }else if(fragmentIndex == 2){
+//                    val gpsFragment = supportFragmentManager.findFragmentByTag("f2") as GpsFragment
+//                    gpsFragment.drawRoute(false, recentBody!!)
+//                }
+//            }
+//        }
+//    }
 
-    // 장치 변경시 그래프 갱신
+    // 장치 변경시 그래프 갱신,, 변경 필요
     @RequiresApi(Build.VERSION_CODES.O)
     private fun changeDevice(fragment:GraphFragment) {
         job = CoroutineScope(Dispatchers.IO).launch {
